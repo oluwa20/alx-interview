73553de3aa3bed260ffcf41700c42a1dc6546fa8
Faisal <oluwabori2004@gmail.com>
2024-08-09 05:17:07 +0100

    All done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,49 +1,21 @@
 #!/usr/bin/python3
 
-
-
 def minOperations(n):
-  
-    pasted_chars = 1  
-    clipboard = 0  
-    counter = 0 
-
-    while pasted_chars < n:
-        
-        if clipboard == 0:
-           
-            clipboard = pasted_chars
-            
-            counter += 1
-
-        
-        if pasted_chars == 1:
-           
-            pasted_chars += clipboard
-           
-            counter += 1
-        
-            continue
-
-        remaining = n - pasted_chars 
-        if remaining < clipboard:
-            return 0
-
-        if remaining % pasted_chars != 0:
-        
-            pasted_chars += clipboard
-            
-            counter += 1
-        else:
-        
-            clipboard = pasted_chars
-    
-            pasted_chars += clipboard
-        
-            counter += 2
-
-
-    if pasted_chars == n:
-        return counter
-    else:
+    if n <= 1:
         return 0
+    
+    operations = 0
+    factor = 2
+    
+    while factor <= n:
+        # If n is divisible by factor, it's a prime factor
+        while n % factor == 0:
+            # Reduce n by factor and add the operation
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
+
+
+
