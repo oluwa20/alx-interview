93977f0dfd200c241870f74307db6e9e41951fa6
Faisal <oluwabori2004@gmail.com>
2024-08-09 04:34:02 +0100

    All done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+
+    if n <= 1:
+        return 0
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+Solutions To Alx 0x02-minimum_operations
